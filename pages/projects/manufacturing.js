cfe0ad96cbf4dab1318959e410c07873b142b606
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-11-15 02:32:03 +0600

    new changes

--- pages/projects/manufacturing.js
+++ pages/projects/manufacturing.js
@@ -1,9 +1,11 @@
+import SeoForAll from "Components/ForSeo/SeoForAll";
 import BrandsServed from "Components/Home/BrandsServed/BrandsServed";
 import ManufacturingDetails from "Components/Projects/ManufacturingDetails";
 import CommonBanner from "Components/sustainability/CommonBanner";
  
 const Manufacturing = () => {
     return ( <>
+    <SeoForAll title='Projects'/>
     <CommonBanner headerBanner={`/Images/projects/manufacturing.png`} title={"MANUFACTURING"}/>
     <ManufacturingDetails/>
     <BrandsServed/>
